5f95e4165c2a84118d9fae366c4477622098faf0
Bipin87 <sahubipin484@gmail.com>
2025-02-23 20:38:01 +0530

    [Bipin Kumar Sahu] Add: Greet with custom attributess feature added🦠🦠🦠

--- My_Greeting_App/src/main/java/com/capgemini/My_Greeting_App/controller/GreetingServiceController.java
+++ My_Greeting_App/src/main/java/com/capgemini/My_Greeting_App/controller/GreetingServiceController.java
@@ -3,6 +3,7 @@ package com.capgemini.My_Greeting_App.controller;
 import com.capgemini.My_Greeting_App.service.GreetingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +19,12 @@ public class GreetingServiceController {
     public String sayHello(){
         return greetingService.getGreetingMessage();
     }
+
+    // GET request to greet with firstname and lastname
+    @GetMapping("/data")
+    public String greetWithFullName(@RequestParam(required = false) String firstName, @RequestParam(required = false) String lastName){
+        return greetingService.customGreetingMessage(firstName,lastName);
+    }
+
 
 }
--- My_Greeting_App/src/main/java/com/capgemini/My_Greeting_App/service/GreetingService.java
+++ My_Greeting_App/src/main/java/com/capgemini/My_Greeting_App/service/GreetingService.java
@@ -7,4 +7,17 @@ public class GreetingService {
     public String getGreetingMessage(){
         return "Hello World";
     }
+
+    // Method to return the hello world using first name and last name
+    public String customGreetingMessage(String firstName, String lastName){
+        if(firstName != null && lastName != null){
+            return "Hello " + firstName + " " + lastName + "!!";
+        } else if (firstName != null) {
+            return "Hello" + firstName + "!!";
+        } else if (lastName != null) {
+            return "Hello" + lastName + "!!";
+        }else {
+            return "Hello World";
+        }
+    }
 }
